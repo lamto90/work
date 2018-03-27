e5c7c7b3e8f3b1e6370a06838f209978246b684f
lamto90 <305645918@qq.com>
2018-03-27 23:45:02 +0800

    resume end

--- js/resume.js
+++ js/resume.js
@@ -1,4 +1,5 @@
 window.onload = function(){
+		
        var first = document.getElementById('first') 
        var lis = first.getElementsByTagName('li')
        var next = document.getElementById('next')
@@ -6,20 +7,21 @@ window.onload = function(){
        var html = document.documentElement
        var fengche = document.getElementById('fengche')
        var num = 0
+   	   var point = 0
        var timer
        var timer1
-       
-     
-       	
-
+	   
+	   window.onbeforeunload = function(){
+          //刷新后页面自动回到顶部
+        html.scrollTop = 0;  //ie下
+        document.body.scrollTop = 0;  //非ie
+ 		}
+   	   lis[0].style.backgroundColor = 'green'
        for (var i = 0; i < lis.length; i++) {
        		lis[i].index = i
-       		lis[i].style.backgroundColor =''
        		lis[i].onclick = function  () { 
-       			
        			clearInterval(timer)
        			clearInterval(timer1)
-       			
 	     		num = this.index
 	     		for (var i = 0; i < lis.length; i++) {
        				lis[i].style.backgroundColor = ''
@@ -37,15 +39,21 @@ window.onload = function(){
  						html.scrollTop = document.body.clientHeight*num
  					}
  				}
- 			},1000/60)
+ 				},1000/60)
 				this.style.backgroundColor = 'green'
+				point = num
 				return false
+				
 			}
    		}
        
    	   next.onclick = function  () {
    			clearInterval(timer)
    			clearInterval(timer1)
+   			point++
+   			if (point>3) {
+   				point = 3
+   			}
    			var flag = html.scrollTop
    			timer1 = setInterval(function  () {
    					html.scrollTop += 20
@@ -53,11 +61,20 @@ window.onload = function(){
    					html.scrollTop = flag + document.body.clientHeight
    				}
    			},1000/60)
+   			for (var i = 0; i < lis.length; i++) {
+   				lis[i].style.backgroundColor = ''
+   			}
+   			lis[point].style.backgroundColor = 'green'
+   		
    		}	
    		
    	   prev.onclick = function  () {
    			clearInterval(timer)
    			clearInterval(timer1)
+   			point--
+   			if (point<0) {
+   				point = 0
+   			}
    			var flag = html.scrollTop
    			timer1 = setInterval(function  () {
 				html.scrollTop -= 20
@@ -65,6 +82,10 @@ window.onload = function(){
 					html.scrollTop = flag - document.body.clientHeight
 				}
    			},1000/60)
+   			for (var i = 0; i < lis.length; i++) {
+   				lis[i].style.backgroundColor = ''
+   			}
+   			lis[point].style.backgroundColor = 'green'
    		}	
 		
 		setTimeout(function  () {
