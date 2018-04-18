b264c9a76e8a30c55d054534cf7dd972a018c3d0
lamto <36362513+lamto90@users.noreply.github.com>
2018-04-18 18:43:43 +0800

    Add files via upload

---
+++ js/jq.js
@@ -0,0 +1,62 @@
+$(function  () {
+	var $lis = $('#first>ul>li')
+	var point = Math.floor($(document).scrollTop()/$(document.body).height())
+	var left = ($('body').width()-$('#prev').width())/2
+	$lis.eq(point).css('background','green')
+	
+	$lis.each(function  (index,item) {
+		$(this).click(function  (event) {
+			$lis.css('background','')
+		    $(this).css('background','green')
+		    point = index
+		move($(document.body).height(),index)
+		event.preventDefault()
+		})
+	})
+	
+	function move (target,flag) {
+		clearInterval(intervalId)
+		var current = $(document).scrollTop()
+		var targetTotal = target*flag
+		//每次scrolltop移动的距离为speed
+		var speed= 15
+		if (current > targetTotal) {
+			speed = -speed
+		}
+		var intervalId = setInterval(function  () {
+			current += speed
+			if ((current >= targetTotal && speed > 0) || (current <= targetTotal  && speed <0) ) {
+				current = targetTotal
+			}
+			if (current === targetTotal) {
+			clearInterval(intervalId)
+			}
+			$(document).scrollTop(current)
+		},1000/60)
+	}
+	
+	$('#prev').click(function  () {
+		point--
+		if (point < 0 ) {
+			point = 0
+		}
+		$lis.css('background','')
+		$lis.eq(point).css('background','green')
+		move($(document).scrollTop()-$(document.body).height(),1)
+	})
+	$('#next').click(function  () {
+		point++
+		if (point > 3 ) {
+			point = 3
+		}
+		$lis.css('background','')
+		$lis.eq(point).css('background','green')
+		move($(document).scrollTop()+$(document.body).height(),1)
+	})
+	
+	$('#prev').css('left',left)
+
+	$('#next').css('left',left)
+
+	
+})
